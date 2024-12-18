043d70badcbf57c3f546422f53e26f8947e79711
moinuddin-4678 <moinuddinshaikh6637@gmail.com>
2024-12-18 20:34:16 +0530

    add lampung

---
+++ greener/src/components/Header.js
@@ -0,0 +1,38 @@
+function Header () {
+    return (
+        <header>
+        <div class="topHead">
+            <div class="logo"><img src="img/logo.png" alt="Lampung Gardening Logo"  /></div>
+            <div class="searchCart">
+                <form action="">
+                    <input type="text" placeholder="Search Plants..." />
+                    <button class="search">
+                        
+                    </button>
+                </form>
+                <a href="" class="cartButton"></a>
+            </div>
+        </div>
+        <nav>
+            <div class="mainNav">                                
+                <a href="#">Home</a>
+                <a href="#">Plant</a>
+                <a href="#">Plant Collection</a>
+                <a href="#">About</a>
+                <a href="#">Blog</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="profileDrop">
+                <span>Hi, Moin</span>
+                <div class="dropDown">
+                    <a href="">Account</a>
+                    <a href="">Setting</a>
+                    <a href="">Log Out</a>
+                </div>
+            </div>
+        </nav>
+    </header>
+    )
+}
+
+export default Header;
---
+++ greener/src/components/Home.js
@@ -0,0 +1,179 @@
+function Home () {
+    return(
+        <>
+         <div class="container">
+        <div class="heading">
+            <h1>make your space<br />
+                <div>
+                    <span>Greener</span>
+                    <img src="" alt="" /> 
+                </div>
+                with 
+                <span>plants</span>
+            </h1>
+            <p>Lorem ispum dolor sit amet, consectetur adipiscing elit .In<br />orci ligula,</p>
+            <div class="buttons">
+                <a href="#" class="firstChild">Shop Plant
+                </a>
+                <a href="#" class="secondChild">About us
+                </a>
+            </div>
+        </div>
+        <div class="rightBoxes">
+            <div class="gridBox">
+                <div sec1>
+                    <h2>Section 1</h2>
+                    <p>Lorem ispum dolor sit amet, consectetur adipiscing<br />elit .In orci ligula. consectetur sit amet</p>
+                    <img src="" alt="" />
+                </div>
+                <div class="sec2"> 
+                    <h2>Section 2</h2>
+                    <p>Lorem ispum dolor sit amet.</p>
+                    <img src="" alt="" />
+                </div>
+                <div class="sec3"> 
+                    <h2>Section 3</h2>
+                    <p>Lorem ispum dolor sit amet.</p>
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+        <section class="greenSec">
+            <div  class="wrapper">
+                <h2>BEST SELLING<br />Plant Collection</h2>
+                {/* <p>Check out our collection, we got every plant you need here!<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let's grow it together </p> */}
+                <div class="container">
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt="" />
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br /> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt="" />
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br /> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/interior-home-plant-in-pot-png.webp" alt="" />
+                        </div>
+                        <div class="info">
+                            <h3>Plant 1</h3>
+                            <p>Lorem Ipsum Dolor<br /> Sit Amet.</p>
+                            <span class="price">$20,00</span>
+                            <a href="" class="button">Buy now</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="imgPara">
+            <div class="img">
+                <img src="../Website 3/img/pngtree-indoor-plant-png-image_6129530.png" alt="tree" />
+            </div>
+            <div class="info">
+                <div class="about">
+                    <h2>What's Best From<br />Our 
+                        <span>Plants</span>
+                    </h2>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br />Quam cum repudiandae sed similique ipsam<br />doloremque iste praesentium est debitis<br />sapientesit atque,libero explicabo impedit<br />quibusdam molestiae commodi ut obcaecati<br />totam sequi earum non ipsa.
+                    </p>
+                </div>
+                <div class="buttons">
+                    <a href="" class="firstChild">Shop Plant
+                        
+                    </a>
+                    <a href="" class="secondChild">Read More
+                       
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="fourthSec">
+            <div class="proverb">
+                <h2>start gardening<br />and grow your<br />own plant!</h2>
+                <img src="../Website 3/img/sand pots.jpg" alt="" />
+                <div class="paraButton">
+                    <p>Lorem ispum dolor sit amet,<br />consectetur adipiscing elit</p>
+                    <a href=""> Read More
+                       
+                    </a>
+                </div>
+            </div>
+        </section>
+        <section class="fifthSec">
+            <div class="aboutHouseplnt">
+                <h2>know more about<br />houseplant</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.<br />Placeat numquam temporibus quaerat ipsum doloribus<br />facere deserunt,autem consequatur ullam ipsam.<br />Donate vulputateorci utmi sodales finibes</p>
+                <div class="buttons">
+                    <a href="" class="firstChild">Our Blog
+                        
+                    </a>
+                    <a href="" class="secondChild">Read More
+                    </a>
+                </div>
+            </div>
+            <div class="img">
+                <img src="" alt="" />
+            </div>
+        </section>
+        <section class="greenSec2">
+            <div  class="wrapper">
+                <h2> what they say<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;reviews</h2>
+                <p>Check out our collection, we got every plant you need here!<br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Let's grow it together </p>
+                <div class="container">
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/bradley.jpg" alt="" />
+                            <h3>Bradley
+                                </h3>
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br />consectetur adipiscing elit.<br />Nam nem eroc ut nunc luctus<br />consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/SAVANNAH.jpg" alt="" />
+                            <h3>Savannah    </h3> 
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br />consectetur adipiscing elit.<br />Nam nem eroc ut nunc luctus<br />consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                    <div class="plantCard">
+                        <div class="thumbnail">
+                            <img src="../Website 3/img/sarah.jpg" alt="" />
+                            <h3>Sarah
+                                </h3>
+                            <div class="bttm"></div>
+                        </div>
+                        <div class="info">
+                            <p>Lorem ispum dolor sit amet,<br />consectetur adipiscing elit.<br />Nam nem eroc ut nunc luctus<br />consecteturvel ac justo</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        </>
+       
+    )
+}
+
+export default Home;
